bcd3fdaa507d168584d8537399e411e15f82582d
Dmitry <smirnoff.spb78@yandex.ru>
2025-01-18 18:31:57 +0300

    Рефакторинг

--- src/main/java/ru/t1/java/demo/exception/ExceptionHandlerApplication.java
+++ src/main/java/ru/t1/java/demo/exception/ExceptionHandlerApplication.java
@@ -1,29 +1,24 @@
 package ru.t1.java.demo.exception;
 
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 
-@ControllerAdvice
-@Slf4j
+@RestControllerAdvice
 public class ExceptionHandlerApplication {
 
     @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity<String> handleResponseStatusException(EntityNotFoundException ex) {
-        return generateErrorResponse(ex, NOT_FOUND);
+    @ResponseStatus(NOT_FOUND)
+    public String handleResponseStatusException(EntityNotFoundException ex) {
+        return ex.getMessage();
     }
 
     @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<String> handleBadRequest(RuntimeException ex) {
-        return generateErrorResponse(ex, BAD_REQUEST);
-    }
-
-    private ResponseEntity<String> generateErrorResponse(Exception ex, HttpStatus httpStatus){
-        return new ResponseEntity<>(ex.getMessage(), httpStatus);
+    @ResponseStatus(BAD_REQUEST)
+    public String handleBadRequest(RuntimeException ex) {
+        return ex.getMessage();
     }
 }
